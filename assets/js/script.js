8edad0471deddf6b311c84939b3f4de841676a2f
Haley T <hntrimpert@yahoo.com>
2023-01-22 16:28:15 -0500

    worked on html, started javascript

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+var startButton = document.getElementById('start-button');
+var questions = document.getElementById('questions');
+var answers = document.getElementById('answers');
+var choice1 = document.getElementById('choice1');
+var choice2 = document.getElementById('choice2');
+var choice3 = document.getElementById('choice3');
+var choice4 = document.getElementById('choice4');
+
+
+
+function createQuestions(){
+
+}
+
+function startQuiz(){
+
+}
+startButton.addEventListener("click", startQuiz);
